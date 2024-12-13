edcba6ad22273f4970e136b32e26a4b2ce321ce5
Zhifan Li <z2367li@uwaterloo.ca>
2024-12-12 22:33:24 -0500

    Reformated app.tsx

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -4,11 +4,11 @@ import Terminal from './components/terminal';
 import './app.css'
 
 const App: React.FC = () => {
-  return (
-    <div>
-      <Terminal />
-    </div>
-  );
+    return (
+      	<div>
+      		<Terminal />
+    	</div>
+  	);
 };
 
 export default App;
